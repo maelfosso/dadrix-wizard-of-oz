91f7f99ac70c4d724f3837568b86e232a4850b30
Mael FOSSO <fosso.mael.elvis@gmail.com>
2022-12-25 15:49:45 +0100

    handle reception of whatsapp message

--- app/src/main/java/com/stockinos/mobile/wizardofoz/WoZApplication.kt
+++ app/src/main/java/com/stockinos/mobile/wizardofoz/WoZApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.util.Log
 import io.socket.client.IO
 import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import org.json.JSONObject
 import java.net.URISyntaxException
 
 class WoZApplication: Application() {
@@ -29,13 +31,23 @@ class WoZApplication: Application() {
             return field
         }
 
+    private var onWhatsappMessageReceived = Emitter.Listener {
+        var data = it[0] as JSONObject
+        Log.d(TAG, "on whatsapp:message:received : $data")
+    }
+
     fun connectSocket() {
         Log.i(TAG, "connectSocket() - ${mSocket == null}")
+
+        mSocket?.on("whatsapp:message:received", onWhatsappMessageReceived)
         mSocket?.connect()
             ?.on(Socket.EVENT_CONNECT) {
                 Log.i(TAG, "connected")
+
+                // sent an authentication message to say that it's an Oscar-ian
             }
             ?.on(Socket.EVENT_DISCONNECT) { Log.i(TAG, "disconnected") }
+
     }
 
     fun disconnectSocket() {
